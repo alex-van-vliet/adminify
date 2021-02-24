bcc1b3ef593f267edc85ec1735ad1b9e9af567b2
Alex van Vliet <alex@vanvliet.pro>
2021-02-24 16:20:14 +0100

    feat: add select2

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="{{ asset('adminify/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('adminify/adminlte/css/adminlte.min.css') }}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('adminify/select2/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminify/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -78,5 +81,15 @@
 <script src="{{ asset('adminify/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('adminify/adminlte/js/adminlte.min.js') }}"></script>
+<!-- Select2 -->
+<script src="{{ asset('adminify/select2/js/select2.full.min.js') }}"></script>
+<script>
+    $(function () {
+        //Initialize Select2 Elements
+        $('.select2bs4').select2({
+            theme: 'bootstrap4'
+        })
+    });
+</script>
 </body>
 </html>
