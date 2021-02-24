2a70e0291488e59bd9e9fe7fbbdee807144d55ea
Alex van Vliet <alex@vanvliet.pro>
2021-02-24 13:54:50 +0100

    feat: handle unique fields

--- src/Fields/StringField.php
+++ src/Fields/StringField.php
@@ -6,6 +6,7 @@ namespace AlexVanVliet\Adminify\Fields;
 
 use Illuminate\Database\Schema\Builder;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 
 class StringField extends Field
 {
@@ -17,6 +18,12 @@ class StringField extends Field
     public function isPassword(): bool
     {
         return Str::contains($this->accessor, 'password');
+    }
+
+    public function isUnique(): bool
+    {
+        return array_key_exists('unique', $this->field->getAttributes())
+            or in_array('unique', $this->field->getAttributes());
     }
 
     public function view(): string
@@ -38,6 +45,8 @@ class StringField extends Field
             $rules [] = 'confirmed';
         if ($this->isEmail())
             $rules [] = 'email';
+        if ($this->isUnique())
+            $rules [] = Rule::unique($this->field->getModel()->getModel()->getTable(), $this->accessor);
         return $rules;
     }
 }
